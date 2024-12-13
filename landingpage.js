7fce48fea2c328c1bee71ee68f66b313edb78a81
memetcina <103679804+newbie778@users.noreply.github.com>
2024-12-13 09:22:35 +0700

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,28 @@
+let availableKeywords = [
+    'Roti Bakar Coklat',
+    'Roti Bakar Selai',
+    'Roti Bakar Matcha'
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+}
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
